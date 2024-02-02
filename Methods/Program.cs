115ea449ae51e7e049e54860ab927a368737c861
Sergoff88 <sergofan88@yandex.ru>
2024-02-02 11:38:11 +0300

    НачалоНачалоДобавьте файлы проекта.

---
+++ Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            int age, numberOfAnimals;
+
+            Console.WriteLine("Укажите Имя");
+
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Укажите Фамилию");
+
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Укажите возраст");
+
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Есть ли питомцы");
+
+            if (Console.ReadLine().ToUpper() == "Да".ToUpper())
+            {
+                Console.WriteLine("Укажите количество питомцев");
+                string[] ArrNicknameAnimals = ArrayNicknameAnimals(Convert.ToInt32(Console.ReadLine()));
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+
+
+        }
+        static string[] ArrayNicknameAnimals(int numberOfAnimals)
+        {
+            string[] array = new string[numberOfAnimals];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Укажите имя {0}-ого питомца", i + 1);
+                array[i] = Console.ReadLine();
+
+            }
+            return array;
+
+        }
+    }
+
+}
+
