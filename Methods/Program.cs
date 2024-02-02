a619a8a1a0999654ca2ba19da45fd3134e7e816c
Sergoff88 <sergofan88@yandex.ru>
2024-02-02 17:13:45 +0300

    +CheckNumber

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Methods
+﻿CheckNumber using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace Methods
 {
     internal class Program
     {
@@ -14,13 +17,13 @@
             lastName = Console.ReadLine();
             //Возраст
             Console.WriteLine("Укажите возраст");
-            age = Convert.ToInt32(Console.ReadLine());
+            age = CheckNumber(Console.ReadLine());
             //Питомцы
             Console.WriteLine("Есть ли питомцы");
             if (Console.ReadLine().ToUpper() == "Да".ToUpper())
             {
                 Console.WriteLine("Укажите количество питомцев");
-                string[] ArrNicknameAnimals = GetArrayFromConsole(Convert.ToInt32(Console.ReadLine()), "Укажите имя {0}-ого питомца");
+                string[] ArrNicknameAnimals = GetArrayFromConsole(CheckNumber(Console.ReadLine()), "Укажите имя {0}-ого питомца");
             }
             else
             {
@@ -39,7 +42,6 @@
             {
                 Console.WriteLine("Укажите имя {0}-ого питомца", i + 1);
                 array[i] = Console.ReadLine();
-
             }
             return array;
 
@@ -71,7 +73,22 @@
 
         }
 
+        static int CheckNumber(string number)
+        {
+            bool check = int.TryParse(number, out int result);
+            if (check == false || result <= 0)
+            {
+                Console.WriteLine("Это не похоже на число, которое больше 0. повторите ввод данных снова");
+                result = CheckNumber(Console.ReadLine());
+            }
+            return result;
+        }
     }
 
-}
+}   // Как лучше поступать плодить больше простого кода (доп переменные, дополнительные методы)? или же делать меньше кода, но на читаемость он будет более сложный
+    //   //Цвета
+    //Console.WriteLine("Сколько у вас любимых цветов?");
+    //string numberOfColors = Convert.ToInt32(Console.ReadLine());
+    //string[] arrFavoriteColors = GetArrayFromConsole(numberOfColors, "Укажите {0}-й любимый цвет");
+
 
